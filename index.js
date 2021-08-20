f85aa6735661eada520cc3167ffe407430b39b50
IhorOsavoluk <84192290+IhorOsavoluk@users.noreply.github.com>
2021-08-20 15:26:35 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
+
+const app = express()
+
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+  try {
+    const url = `mongodb+srv://IhorOsavoluk:0506356450Igor@dveri.cpuqk.mongodb.net/shop`
+     await mongoose.connect("mongodb://localhost:27017", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    pass: "pass",
+    user: "name",
+  });
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+start()
+
